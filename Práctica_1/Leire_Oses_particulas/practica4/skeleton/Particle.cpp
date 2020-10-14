9cfa2f7e44aca4dcee102b02f77d78c66f3d8020
Leyre Osés Sánchez <48771457+leoses@users.noreply.github.com>
2020-10-14 09:58:35 +0200

    Parte 4 terminada

---
+++ Práctica_1/Leire_Oses_particulas/practica4/skeleton/Particle.cpp
@@ -0,0 +1,30 @@
+#include "Particle.h"
+
+Particle::~Particle()
+{
+	rItem->release();
+}
+
+void Particle::integrate(float t)
+{
+	move(t);
+
+	// Update linear velocity
+	velocity_ += acceleration_ * t;
+	// Impose drag (damping)
+	velocity_ *= powf(damping, t);
+
+}
+
+void Particle::move(float t)
+{
+	// Trivial case, infinite mass --> do nothing
+	if (inverse_mass <= 0.0f) return;
+	// Update position
+	position_ += velocity_ * t;
+
+	//Modificar el transform del renderItem
+	trans = PxTransform(position_);
+	rItem->transform = &trans;
+}
+
